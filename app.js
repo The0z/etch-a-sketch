5c6812d9f65d442a7e7258919e6e2e8a796bd8b9
The0z <mraustingallagher@gmail.com>
2022-09-04 17:56:10 -0600

    Add 16x16 Grid with JS&CSS

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 function div16GridCreator (){
+    const body = document.querySelector('#content');
+    const gridDiv = document.createElement('div');
+
+    gridDiv.setAttribute('id', 'gridDiv'); 
+
+    //16x16 grid
+    for (let i = 1; i <= 256; i++){
+        const gridBox = document.createElement('div');
+    
+        gridBox.setAttribute('background-color', 'red');
+        gridBox.classList.add('gridSquare');
+        gridBox.setAttribute('id',`grid${i}`);
+        gridDiv.appendChild(gridBox);
+    }
+    body.appendChild(gridDiv);
 
 }
 
