81faa3c372dff89b8340c248b93ddf6865992207
The0z <mraustingallagher@gmail.com>
2022-09-04 19:09:37 -0600

    Add Resize grid function

--- app.js
+++ app.js
@@ -30,7 +30,20 @@ document.querySelector('#content').addEventListener('mouseover', function(e){
 } );
 
 document.querySelector('#resetBtn').addEventListener('click', resetGame);
+document.querySelector('#gridBtn').addEventListener('click', resizeGrid);
 
 function resetGame(){
     Array.from(document.querySelectorAll('.gridSquare')).forEach((e) => e.classList.remove('etchColor'));
+}
+
+function resizeGrid(){
+    let gridLength = prompt('Enter a Grid Length Between 1 and 100');
+    let content = document.querySelector('#content');
+    content.removeChild(content.firstChild);
+    if (gridLength <= 100 && gridLength > 0){
+        divGridCreator(gridLength);
+    } else {
+        alert("INVALID SIZE! Grid Length Set to 32");
+        divGridCreator(32);
+    }
 }
